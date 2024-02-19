04f87de734792b0ca75f9a28c3c2a7002d7e3b80
Johnny8822 <146615330+Johnny8822@users.noreply.github.com>
2024-02-18 19:48:57 -0500

    add patch

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request,   Response, status 
 
+import uuid
 app = FastAPI()
 
 tanks = [] 
@@ -12,5 +13,28 @@ def get_tank():
 
 @app.get("/tank{tank_id}") 
 def get_tank_id(): 
-   tank_num = 0 
-   for j in range len(tanks)
+     tank_num = 0 
+#    for j in range (len(tanks)):
+    
+
+
+
+@app.post("/tank") 
+async def post_tank(request: Request, response: Response):
+   tank = await request.json 
+   new_uuid = uuid.uuid4() 
+   
+   tank = {"id": str(new_uuid), **tank}
+   tanks.append(tank)  
+   response.status_code  = status.HTTP_201_CREATED
+   return(tank)
+
+
+@app.patch("/tank/{id}")
+async def patch_tank(id: str, request:Request):
+    patched_tank = await request.json()
+
+    for i, tank in enumerate(tanks):
+        if tank["id"] == id:
+            tanks[i] = {**tank, **patched_tank}
+            return tanks[i]
